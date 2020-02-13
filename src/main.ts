f3710bb75b40a2aff86766b67cd4a0bf4cc6c590
Naveen Naidu <__naveen.naidu@ushur.com>
2020-02-13 15:24:35 +0530

    Test:1

--- src/main.ts
+++ src/main.ts
@@ -15,46 +15,7 @@ async function run() {
         if ( context.payload.pull_request.body.indexOf( bodyContains) < 0  ) {
             core.setFailed("The body of the PR does not contain " + bodyContains)
         }
-
-        const bodyDoesNotContain = core.getInput('bodyDoesNotContain')
-        if ( bodyDoesNotContain && context.payload.pull_request.body.indexOf( bodyDoesNotContain) >= 0  ) {
-            core.setFailed("The body of the PR should not contain " + bodyDoesNotContain);
-        }
         
-	const diffContains = core.getInput('diffContains')
-	const diff_url = context.payload.pull_request.diff_url
-	const result = await github.request( diff_url )
-	const files = parse(result.data)
-	const filesChanged = +core.getInput('filesChanged')
-	if ( filesChanged && files.length != filesChanged ) {
-            core.setFailed( "You should change exactly " + filesChanged + " file(s)");
-	}
-
-	var changes = ''
-	var additions:number = 0
-	files.forEach(function(file) {
-	    additions += file.additions
-            file.chunks.forEach( function ( chunk ) {
-		chunk.changes.forEach( function (change ) {
-                    if ( change.add ) {
-			changes += change.content
-                    }
-		})
-            })
-	})
-	if ( changes.indexOf( diffContains ) < 0 ) {
-            core.setFailed( "The added code does not contain " + diffContains);
-	} else {
-            core.exportVariable('diff',changes )
-            core.setOutput('diff',changes )
-	}
-
-	const linesChanged = +core.getInput('linesChanged')
-	if ( linesChanged && ( additions != linesChanged ) ) {
-	    const this_msg = "You should change exactly " + linesChanged + " lines(s) and you have changed " + additions
-            core.setFailed( this_msg );
-	}
-
     } catch (error) {
 	core.setFailed(error.message);
     }
